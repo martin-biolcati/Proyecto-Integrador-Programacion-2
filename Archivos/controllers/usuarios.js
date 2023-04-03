d36ff93465c7b833e6df7a3758c6a4a23a26c3a9
martin-biolcati <martinbiolcati@gmail.com>
2023-04-03 17:12:50 -0300

    avances formularios y rutas

---
+++ Archivos/controllers/usuarios.js
@@ -0,0 +1,9 @@
+const controlador = {
+    register: function(req,res){
+        res.render(
+            'register'
+        )
+    }
+}
+
+module.exports = controlador;
--- Archivos/routes/users.js
+++ Archivos/routes/users.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const usuarios = require('../controllers/usuarios')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.get('/register/', usuarios.register)
 module.exports = router;
