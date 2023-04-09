f52d2498168bdd7b9890d57e2b9b9fd3ca82a3a3
gonzalo-lemoine <glemoine@udesa.edu.ar>
2023-04-09 19:17:52 -0300

    pruebas

--- Archivos/controllers/index.js
+++ Archivos/controllers/index.js
@@ -1,10 +1,9 @@
-const data = require('../data/data')
 
 const controller ={
     index: function(req,res){
         res.render('index',{
-            courses: data.courses,
-            userLogueado: true
+          
+            usuarioLogueado: true,
         })
     }
 }
