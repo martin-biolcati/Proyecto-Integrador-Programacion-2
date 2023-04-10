3b65f06c2d6c3204d11ca7126206d26601e092d3
BautistaGiorgi <jgiorgi@udesa.edu.ar>
2023-04-10 14:47:24 -0300

    data productos

--- Archivos/db/data/data.js
+++ Archivos/db/data/data.js
@@ -1,17 +1,66 @@
  const data = {
-    courses: [
+    productos: [
         {
-           nombre: 'programacion2',
-            imgUrl: '',
-            precio: '15usd',
-            description: 'hola',
-            comments:[
-                {
-                    usuario: 'pepe',
-                    comentario:  'hola ',
-                }
+           id:1,
+           producto: "Porsche 911 Carrera",
+           anio: 2018,
+           descripcion: 'Auto usado por 1 anio en muy buen estado.',
+           image: '/imgen'
+        },{
+            id:2,
+            producto: "LAMBORGHINI AVENTADOR SVJ ",
+            anio: 2022,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:3,
+            producto: "FERRARI 812 COMPETIZONE",
+            anio: 2023,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:4,
+            producto: "AUDI RSQ8",
+            anio: 2022,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:5,
+            producto: "FORD RAPTOR ",
+            anio: 2020,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:6,
+            producto: "BMW M8",
+            anio: 2021,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:7,
+            producto: "LOTUS EMIRA",
+            anio: 2023,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:8,
+            producto: "ROLLS ROYCE PHANTOM",
+            anio: 2023,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:9,
+            producto: "PAGANI HUAYRA",
+            anio: 2022,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        },{
+            id:10,
+            producto: "MERCEDES BENZ AMG GT R PRO",
+            anio: 2022,
+            descripcion: 'Auto sin usar en perfectas condiciones.',
+            image: '/imgen'
+        }
+                
             ]
         }
-    ]
-
-}
