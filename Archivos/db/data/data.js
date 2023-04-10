f38eb1d911223f61a1a40461f705cb42207d8ae6
BautistaGiorgi <jgiorgi@udesa.edu.ar>
2023-04-10 20:34:04 -0300

    datos y sql

--- Archivos/db/data/data.js
+++ Archivos/db/data/data.js
@@ -1,67 +1,205 @@
  const data = {
-    productos: [
-        {
-           id:1,
-           nombre: "Porsche 911 GT3",
-           anio: 2022,
-           descripcion: 'Auto usado por 1 anio en muy buen estado.',
-           image: '/images/products/porsche_911.jpg',
-        },{   
-            id:2,
-            nombre: "LAMBORGHINI AVENTADOR SVJ ",
-            anio: 2022,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/lambo.jpg'
-        },{
-            id:3,
-            nombre: "FERRARI 812 COMPETIZONE",
-            anio: 2023,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/ferrari_812_competizione.jpg'
-        },{
-            id:4,
-            nombre: "AUDI RSQ8",
-            anio: 2022,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/audi_rsq8.jpg'
-        },{
-            id:5,
-            nombre: "FORD F-150 RAPTOR ",
-            anio: 2020,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/ford_raptor.jpg'
-        },{
-            id:6,
-            nombre: "BMW M8",
-            anio: 2021,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/m8.jpg'
-        },{
-            id:7,
-            nombre: "LOTUS EMIRA",
-            anio: 2023,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/lotus.jpg'
-        },{
-            id:8,
-            nombre: "ROLLS ROYCE PHANTOM",
-            anio: 2023,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/phantom.jpg'
-        },{
-            id:9,
-            nombre: "PAGANI HUAYRA",
-            anio: 2022,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/pagani_huayra.jpg'
-        },{
-            id:10,
-            nombre: "Lamborghini Urus",
-            anio: 2022,
-            descripcion: 'Auto sin usar en perfectas condiciones.',
-            image: '/images/products/lamborghini_urus.jpg'
-        }
-                
-            ]
+        usuario: [
+            {
+                id: 1,
+                nombre:'nelson', 
+                email:'nelson@gmail.com', 
+                password:'pepegrillo',
+                foto_de_perfil: 'images/',
+                dni: 9435223,
+                fecha_de_nacimiento: '2002-02-13'
+            },{
+                id: 2, 
+                nombre:'martin', 
+                email:'martin@gmail.com', 
+                password:'martin123',
+                foto_de_perfil:'images/',
+                dni: 53432425,
+                fecha_de_nacimiento: '2003-04-22'
+            },{
+                id: 3, 
+                nombre:'bautista', 
+                email:'bauti@gmail.com', 
+                password:'bauti2345',
+                foto_de_perfil:'images/',
+                dni: 3445634,
+                fecha_de_nacimiento: '2003-11-11'
+            },{
+                id: 4, 
+                nombre:'juan', 
+                email:'juancho@gmail.com', 
+                password:'juanchito',
+                foto_de_perfil:'images/',
+                dni: 45423634,
+                fecha_de_nacimiento: '2010-02-23'
+            },{
+                id: 5, 
+                nombre:'pepe', 
+                email:'pepito@gmail.com', 
+                password:'pepe123',
+                foto_de_perfil:'images/',
+                dni: 9453456,
+                fecha_de_nacimiento: '2001-03-30'
+            },{
+                id: 6,
+                nombre:'ana', 
+                email:'anita@gmail.com', 
+                password:'anita234',
+                foto_de_perfil:'images/',
+                dni: 4352328,
+                fecha_de_nacimiento: '1993-01-11'
+            },{
+                id: 7, 
+                nombre:'andrea', 
+                email:'andrea@gmail.com', 
+                password:'andrea7388',
+                foto_de_perfil:'images/',
+                dni: 4656734,
+                fecha_de_nacimiento: '1982-02-04'
+            },{
+                id: 8,
+                nombre:'manuel', 
+                email:'manuel@gmail.com', 
+                password:'manuel2003',
+                foto_de_perfil:'images/',
+                dni: 9456345,
+                fecha_de_nacimiento: '2003-06-16'
+            },{
+                id: 9,
+                nombre:'sofia', 
+                email:'sofia@gmail.com', 
+                password:'sofi939',
+                foto_de_perfil:'images/',
+                dni: 25568540,
+                fecha_de_nacimiento: '2000-11-11'
+            },{ 
+                id: 10,
+                nombre:'gonzalo', 
+                email:'gonza@gmail.com', 
+                password:'gonzalito23',
+                foto_de_perfil:'images/',
+                dni: 58643205,
+                fecha_de_nacimiento: '2004-05-29'
+            }
+                ],
+        productos: [
+            {
+                id:1,
+                nombre: "PORSCHE 911 GT3",
+                fecha_de_carga: 2022,
+                descripcion: 'Auto usado por 1 anio en muy buen estado.',
+                image: '/images/products/porsche_911.jpg',
+            },{   
+                id:2,
+                nombre: "LAMBORGHINI AVENTADOR SVJ",
+                fecha_de_carga: 2022,
+                descripcion: 'Auto sin usar en perfectas condiciones.',
+                image: '/images/products/lambo.jpg'
+            },{
+                id:3,
+                nombre: "FERRARI 812 COMPETIZONE",
+                fecha_de_carga: 2023,
+                descripcion: 'Auto usado por 2 anios en muy buen estado.',
+                image: '/images/products/ferrari_812_competizione.jpg'
+            },{
+                id:4,
+                nombre: "AUDI RSQ8",
+                fecha_de_carga: 2022,
+                descripcion: 'Auto sin usar en perfectas condiciones.',
+                image: '/images/products/audi_rsq8.jpg'
+            },{
+                id:5,
+                nombre: "FORD F-150 RAPTOR",
+                fecha_de_carga: 2020,
+                descripcion: 'Auto sin usar en perfectas condiciones.',
+                image: '/images/products/ford_raptor.jpg'
+            },{
+                id:6,
+                nombre: "BMW M8",
+                fecha_de_carga: 2021,
+                descripcion: 'Auto sin usar en perfectas condiciones.',
+                image: '/images/products/m8.jpg'
+            },{
+                id:7,
+                nombre: "LOTUS EMIRA",
+                fecha_de_carga: 2023,
+                descripcion: 'Auto usado por 1 anio en muy buen estado.',
+                image: '/images/products/lotus.jpg'
+            },{
+                id:8,
+                nombre: "ROLLS ROYCE PHANTOM",
+                fecha_de_carga: 2023,
+                descripcion: 'Auto sin usar en perfectas condiciones.',
+                image: '/images/products/phantom.jpg'
+            },{
+                id:9,
+                nombre: "PAGANI HUAYRA",
+                fecha_de_carga: 2022,
+                descripcion: 'Auto usado por 3 anios en muy buen estado.',
+                image: '/images/products/pagani_huayra.jpg'
+            },{
+                id:10,
+                nombre: "Lamborghini Urus",
+                fecha_de_carga: 2022,
+                descripcion: 'Auto sin usar en perfectas condiciones.',
+                image: '/images/products/lamborghini_urus.jpg'
+            }
+                    
+                ],
+        comentarios: [
+            {
+                id:1,
+                nombre: '',
+                comentario: 'Muy lindo auto',
+                imagen_de_perfil:''  
+             },
+             {
+                id:2,
+                nombre: '',
+                comentario: 'Le compre el auto a mi mama y le encanto. Muy recomendable.',
+                imagen_de_perfil:''  
+             }, {
+                id:3,
+                nombre: '',
+                comentario: 'Lo compraria pero tiene mucho uso.',
+                imagen_de_perfil:''  
+             }, {
+                id:4,
+                nombre: '',
+                comentario: 'Hermoso auto. Lo compraria pero es muy caro',
+                imagen_de_perfil:''  
+             }, {
+                id:5,
+                nombre: '',
+                comentario: 'Auto recomendable para conductores avanzados',
+                imagen_de_perfil:''  
+             },{
+                id:6,
+                nombre: '',
+                comentario: 'Lo quiero!!',
+                imagen_de_perfil:''  
+             },
+             {
+                id:7,
+                nombre: '',
+                comentario: 'Lo compre pero no me convencio.',
+                imagen_de_perfil:''  
+             }, {
+                id:8,
+                nombre: '',
+                comentario: 'Genial su ingenieria',
+                imagen_de_perfil:''  
+             }, {
+                id:9,
+                nombre: '',
+                comentario: 'Me gustaria probarlo.',
+                imagen_de_perfil:''  
+             }, {
+                id:10,
+                nombre: '',
+                comentario: 'Auto recomendable para familia.',
+                imagen_de_perfil:''  
+             }
+        ]
         }
 module.exports = data
